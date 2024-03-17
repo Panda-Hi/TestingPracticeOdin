0ff72e2c8c8a6371e02b96d8d996c3df18ddbcb2
Panda-Hi <118573854+Panda-Hi@users.noreply.github.com>
2024-03-17 18:25:13 +0100

    feat: add capitalize function

---
+++ index.js
@@ -0,0 +1,7 @@
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+capitalize("egg");
+const tes = "abc";
+console.log(tes[1].toUpperCase());
+module.exports = { capitalize };
