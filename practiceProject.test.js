b73093b1955548d42a9717dad405048d08f4dbd5
Panda-Hi <118573854+Panda-Hi@users.noreply.github.com>
2024-03-19 11:40:50 +0100

    test: add Jest tests for reverseString function

--- practiceProject.test.js
+++ practiceProject.test.js
@@ -1,4 +1,17 @@
-const { capitalize } = require("./index");
+const { capitalize, reverseString } = require("./index");
+
+describe("reverse string function", () => {
+  it("reverse word", () => {
+    const string = "monkey";
+    const expectedVal = "yeknom";
+    expect(reverseString(string)).toBe(expectedVal);
+  });
+  it("reverse letter", () => {
+    const letter = "d";
+    const expectedVal = "d";
+    expect(reverseString(letter)).toBe(expectedVal);
+  });
+});
 
 describe("capitalize function", () => {
   it("capitalize first letter of string", () => {
