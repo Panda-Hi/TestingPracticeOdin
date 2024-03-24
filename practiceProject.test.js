898a23d614c67df6382c47222c4715d5937a7bb9
Panda-Hi <118573854+Panda-Hi@users.noreply.github.com>
2024-03-24 17:54:41 +0100

    test: add jest test for calculator object
    tests check add, subtract, divide and multiply
    functions from calculator object

--- practiceProject.test.js
+++ practiceProject.test.js
@@ -1,5 +1,31 @@
-const { capitalize, reverseString } = require("./index");
+const { capitalize, reverseString, calculator } = require("./index");
 
+describe("calculator function", () => {
+  it("add a + b", () => {
+    const a = 5.1;
+    const b = 3.5;
+    const expectedVal = 8.6;
+    expect(calculator.add(a, b)).toBeCloseTo(expectedVal);
+  });
+  it("subtract a - b", () => {
+    const a = 5;
+    const b = 3.5;
+    const expectedVal = 1.5;
+    expect(calculator.subtract(a, b)).toBeCloseTo(expectedVal);
+  });
+  it("divide a / b", () => {
+    const a = 5.5;
+    const b = 1.1;
+    const expectedVal = 5;
+    expect(calculator.divide(a, b)).toBeCloseTo(expectedVal);
+  });
+  it("multiply a * b", () => {
+    const a = 5.5;
+    const b = 10;
+    const expectedVal = 55;
+    expect(calculator.multiply(a, b)).toBeCloseTo(expectedVal);
+  });
+});
 describe("reverse string function", () => {
   it("reverse word", () => {
     const string = "monkey";
